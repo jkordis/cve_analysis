15d90d70fd1a39e005e11171013501331452d01d
jkordis <104942170+jkordis@users.noreply.github.com>
2023-04-17 07:48:35 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# CADE - CVE Analysis and Dread Evaluation 
+
+This is an open source streamlit application I'm building to test out building a functional web tool that can be used to assess impact easily.
+
+
+## Authors
+
+- John Kordis
+
+## Roadmap
+
+- CVE Analysis, details, cve number, cvss score, epss score
+
+- Dread Analysis 
+
+- CISA Known Exploited addition
+
+
---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import requests
+import json
+import pandas as pd
+
+# Create a Streamlit interface
+st.sidebar.title("CVE Lookup")
+cve = st.sidebar.text_input("Enter CVE Number")
+
+if st.sidebar.button("Submit"):
+    if cve:
+        # Add a loading bar
+        with st.spinner("Searching for vulnerability details..."):
+            # Send a GET request to the NVD API
+            url = f"https://services.nvd.nist.gov/rest/json/cve/1.0/{cve}"
+            response = requests.get(url)
+
+            # Convert response to a dictionary
+            data = json.loads(response.text)
+
+            # Display vulnerability details
+            if "result" in data:
+                result = data["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]
+                title = data["result"]["CVE_Items"][0]["cve"]["CVE_data_meta"]["ID"]
+                cvss_score = result["cvssV2"]["baseScore"]
+                st.write(f"Title: {title}")
+                st.write(f"CVSS Score: {cvss_score}")
+            else:
+                st.write("No results found for the given CVE number.")
+    else:
+        st.write("Please enter a CVE number.")
